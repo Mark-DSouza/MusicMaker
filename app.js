eb76a2139668cd3f6c24e078df035959d5acc5c1
Mark D'Souza <markdsouza434@gmail.com>
2020-08-11 18:04:41 +0530

    Commit MVP of App

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-// Selector
+window.addEventListener('load', () => {
+    // Selectors
+    sounds = document.querySelectorAll('.sound');
+    pads = document.querySelectorAll('.pads div');
+    // colours = [
+    //     '#60d394', 
+    //     '#d36060', 
+    //     '#c060d3', 
+    //     '#d3d160', 
+    //     '#6860d3', 
+    //     '#60b2d3'
+    // ]
+
+    // Event Listeners
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', () => {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
 
 
 
-// Event Listeners
+    // Functions
+})
 
 
-
-
-// Functions
